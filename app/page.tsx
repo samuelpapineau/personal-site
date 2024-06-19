e3863d2516774afd1261bf31d33f8dca7f0a18d4
Samuel Papineau <samuelpapineau08@gmail.com>
2024-06-18 21:17:46 -0400

    Added about and resume sections

--- app/page.tsx
+++ app/page.tsx
@@ -19,10 +19,33 @@ export default function Home() {
 			{/*<Header />*/}
 			<Navbar />
 			<div className="profile-container">
-				<h1 className="profile-name">Samuel Papineau</h1>
+				<h1 className="profile-name">Hi! I'm Sam</h1>
 				<p className="profile-description">Front-end web developer, web designer, and hobby software developer.</p>
-				<button className="profile-button">About Me</button>
+				<button className="profile-button">Contact Me</button>
 				<img className="profile-picture" src="image/profile.jpg" alt="Sam in a canoe" />
+			</div>
+			<div id="about"className="about">
+				<p className="about-title">
+					About Me:
+				</p>
+
+				<p className="about-content">
+					I'm a self-taught frontent web developer with 10 years of hobby experience and two years of professional experience. 
+					I have experience developing APIs, discord bots, web apps, and more. While my main focus is frontend, 
+					I have backend experience from working with various database management systems, data sinks, and several personal projects.
+				</p>
+				<div className="about-list">
+					<li>Built an API that parsed transaction data from EOS, Telos, and Wax blockchains for the
+					<a className="about-link" href="spyglass.network" target="_blank"> Spyglass Network website</a> as mentioned in the projects section.</li>
+					<li>Developed a Discord bot for the EOS Community Discord server, which distributed POP
+					Token NFTs to users who ran a command that included their Anchor account username.</li>
+					<li>Created a tool that pipes data from a blockchain to a WebSocket connection using
+					Substreams: <a className="about-link" href="" target="_blank">GitHub Repository.</a></li>
+					<li>Worked on a data sink that extracts, parses, and validates Substreams inscriptions data and
+					passes it to an API: <a className="about-link" href="" target="_blank">GitHub Repository.</a></li>
+				</div>
+
+				<p>My skills:</p>
 			</div>
 			<div className="projects-container">
 				<hr className="hr"></hr>
@@ -34,10 +57,10 @@ export default function Home() {
 				<br></br>
 				{/*Remove br elements later*/}
 				<h1 className="projects-title">Some of my <br></br>projects:</h1>
-				<div className="card-container">
-					<div className="red"><Card data="public/projects/Mooshrum_Bot.json" /></div>
-					<div className="red"><Card data="public/projects/Math_Game.json" /></div>
-					<div className="red"><Card data="public/projects/Spyglass.json" /></div>
+				<div id="projects" className="card-container">
+					<div><Card data="public/projects/Mooshrum_Bot.json" /></div>
+					<div><Card data="public/projects/Math_Game.json" /></div>
+					<div><Card data="public/projects/Spyglass.json" /></div>
 				</div>
 			</div>
 			<div className="canoe-footer">
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -6,11 +6,12 @@ export const Navbar = () => {
 		<>
 			<div className="navbar">
 				<ul className="ul-nav">
-					<li className="li-nav"><a href="#home">Home</a></li>
+					{/*<li className="li-nav"><a href="#home">Home</a></li>*/}
+					<li className="li-nav"><a href="#about">About</a></li>
 					<li className="li-nav"><a href="#projects">Projects</a></li>
 					{/*<li className="li-nav"><a href="#about">Blog</a></li>*/}
-					<li className="li-nav"><a href="#about">About</a></li>
-					<li className="li-nav"><a href="#contact">Contact</a></li>
+					{/*<li className="li-nav"><a href="#contact">Contact</a></li>*/}
+					<li className="li-nav"><a href="documents\Resume-Samuel-Papineau.pdf" target="_blank">Resume</a></li>
 				</ul>
 
 			</div>
